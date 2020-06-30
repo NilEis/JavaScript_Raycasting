93da8f39024aed502adf643e62a7d1e0d10c3d19
NilEis <NilEis@users.noreply.github.com>
2020-06-30 14:33:33 +0200

    added array-based stuff

--- JS/Raycasting/functions.js
+++ JS/Raycasting/functions.js
@@ -2,58 +2,6 @@
  * @file Stellt Funktionen zum erstellen von und interagieren mit dem Raycaster
  * @module RaycastFunctions.js
  */
-/**
- * Zeichnet alle Wände
- */
-function drawWalls() {
-    for (let i = 0; i < walls.length; i++)
-        walls[i].draw();
-}
-
-/**
- * Erstellt die äußeren Wände
- */
-function setBounds() {
-    walls.push(new wall(0, 0, 0, 512, 255, 0, 0));
-    walls.push(new wall(0, 0, 512, 0));
-    walls.push(new wall(512, 512, 0, 512));
-    walls.push(new wall(512, 512, 512, 0));
-}
-
-/**
- * Erzeugt ein Rechteck aus Wänden mit den gegebenen Maßen und in der gegebenen Farbe
- * @param {number} x Die x-Startposition
- * @param {number} y Die y-Startposition
- * @param {number} w Die Breite
- * @param {number} h Die Höhe
- * @param {number} r Der Anteil an roter Farbe (0-255)
- * @param {number} g Der Anteil an grüner Farbe (0-255)
- * @param {number} b Der Anteil an blauer Farbe
- */
-function addRect(x, y, w, h, r, g, b) {
-    walls.push(new wall(x, y, x + w, y, r, g, b));
-    walls.push(new wall(x, y, x, y + h, r, g, b));
-    walls.push(new wall(x, y + h, x + w, y + h, r, g, b));
-    walls.push(new wall(x + w, y + h, x + w, y, r, g, b));
-}
-
-/**
- * Erzeugt ein Rechteck aus Wänden mit den gegebenen Maßen und in der gegebenen Farbe, die allerding durch die gegebene Textur ünerzeichnet wird
- * @param {number} x Die x-Startposition
- * @param {number} y Die y-Startposition
- * @param {number} w Die Breite
- * @param {number} h Die Höhe
- * @param {number} r Der Anteil an roter Farbe (0-255)
- * @param {number} g Der Anteil an grüner Farbe (0-255)
- * @param {number} b Der Anteil an blauer Farbe
- * @param {string} imgSrc Der Pfad zur Textur
- */
-function addTextRect(x, y, w, h, r, g, b, imgSrc) {
-    walls.push(new wall(x, y, x + w, y, r, g, b, imgSrc));
-    walls.push(new wall(x, y, x, y + h, r, g, b, imgSrc));
-    walls.push(new wall(x, y + h, x + w, y + h, r, g, b, imgSrc));
-    walls.push(new wall(x + w, y + h, x + w, y, r, g, b, imgSrc));
-}
 
 /**
  * Die Funktion, die die Tasteneingaben verarbeitet
@@ -90,4 +38,9 @@ function buttons() {
             _emitter.setDir(_emitter.o);
             break;
     }
+}
+
+function drawMap() {
+    c.fillRect(WIDTH / 2, HEIGHT / 2, WIDTH / 2, HEIGHT / 2, "grey");
+    c.fillArray1D(map, sizeX, ["black", "white"], (c.width / 2) / sizeX, c.height / sizeY);
 }
--- JS/Raycasting/vars.js
+++ JS/Raycasting/vars.js
@@ -1,5 +1,53 @@
-const pv = 5;
-const PI = Math.PI;;
-const P2 = PI / 2;
-const P3 = 3 * PI / 2;
-const DR = 0.0174533;
+/**
+ * @file Stellt Konstanten für das Spiel zur verfügung
+ * @module constants
+ */
+/**
+ * @constant {number} WIDTH Die Anzahl aller Spalten
+ */
+const WIDTH = 1024;
+/**
+ * @constant {number} HEIGHT Die Anzahl aller Zeilen
+ */
+const HEIGHT = 512;
+/**
+ * @constant {number} TILESIZE Die Breite und Höhe jeder Zelle
+ */
+const TILESIZE = 1;
+/**
+ * @constant {number} FPS Die Anzahl der aktualisierungen pro Sekunde
+ */
+const FPS = 60;
+/**
+ * @constant {number[]} map Das Spielfeld
+ */
+const map = [
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 1, 0, 0, 1,
+    1, 0, 0, 0, 0, 1, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1
+];
+/**
+ * @constant {number} sizeX Die Breite einer Reihe
+ */
+const sizeX = 8;
+/**
+ * @constant {number} sizeY Die Höhe einer Spalte
+ */
+const sizeY = 8;
+/**
+ * @constant {number} sizeM Die Gesamtgröße des Feldes
+ */
+const sizeM = sizeX * sizeY;
+/**
+ * @constant {number} sizeTX Die Breite einer Zelle
+ */
+const sizeTX = (WIDTH / 2) / sizeX;
+/**
+ * @constant {number} sizeTY Die Höhe einer Zelle
+ */
+const sizeTY = HEIGHT / sizeY;
